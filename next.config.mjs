9522792a3d7733a708317176677ca28191104a1c
Aleksandr Mochalov <18alex09@mail.ru>
2024-07-31 17:52:39 +0500

    N3

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'fakestoreapi.com',
-            port: '',
-            pathname: '/account123/**',
-          },
-        ],
-      },
+    output: "export",
 };
 
 export default nextConfig;
